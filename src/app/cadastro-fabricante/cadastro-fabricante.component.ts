859582e8d1887bf39ff8f26af88e2246ace297e5
Daniel Lira <liravoz@gmail.com>
2019-04-10 13:10:59 -0300

    Projeto

---
+++ src/app/cadastro-fabricante/cadastro-fabricante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-fabricante',
+  templateUrl: './cadastro-fabricante.component.html',
+  styleUrls: ['./cadastro-fabricante.component.css']
+})
+export class CadastroFabricanteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pesquisa-fabricante/pesquisa-fabricante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pesquisa-fabricante',
+  templateUrl: './pesquisa-fabricante.component.html',
+  styleUrls: ['./pesquisa-fabricante.component.css']
+})
+export class PesquisaFabricanteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
